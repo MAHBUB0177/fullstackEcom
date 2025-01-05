e903285053ccdbea098870aa61a4a0c8a0a94b03
mahbub alam <ma01776879@gmail.com>
2025-01-05 10:32:23 +0600

    ok

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,27 @@
 // authUtils.ts
+// import { RootState } from "@/store";
+// import { store } from "@/store"; // Ensure you export your store properly
+
+// export const getAuthData = (): AuthDataType | null => {
+//   const state = store.getState() as RootState;
+//   return state.auth.authData || null;
+// };
+
+
 import { RootState } from "@/store";
 import { store } from "@/store"; // Ensure you export your store properly
 
+
+ interface AuthDataType {
+    accessToken: string;
+    refreshToken: string;
+    refreshTokenExpiration: string;
+    tokenExpiration: string;
+  }
+  
 export const getAuthData = (): AuthDataType | null => {
-  const state = store.getState() as RootState;
-  return state.auth.authData || null;
+    const state = store.getState(); // Access Redux store directly
+      const authData = state.auth.authData as AuthDataType;
+  
+  return authData;
 };
-
-
