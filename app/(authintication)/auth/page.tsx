f7b3726c884e04ded18a1ca940bf4c94e0a0c59b
@mahbub.alam <mahbub.alam@technonext.com>
2025-01-31 22:39:41 +0600

    remove console

--- app/(authintication)/auth/page.tsx
+++ app/(authintication)/auth/page.tsx
@@ -76,7 +76,6 @@ const Login = () => {
   useEffect(() => {
     const { searchParams } = new URL(window.location.href);
     const callbackUrl = searchParams.get('callbackUrl');
-    console.log(callbackUrl,'callbackUrl====')
     if (sessionStatus === "authenticated") {
       if (callbackUrl) {
         router.replace(callbackUrl);
